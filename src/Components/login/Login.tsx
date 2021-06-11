89bb9f17c8aacb64d7024c079abccffc29db4460
rugaba <rugaba@gmail.com>
2021-06-11 09:26:26 +0200

    add tailwindcss

--- src/Components/login/Login.tsx
+++ src/Components/login/Login.tsx
@@ -16,8 +16,15 @@ export default function Login() {
    },[dispatch])
     
     return (
-        <div>
-            hello world! {emailReducer.email}
+    
+           
+        <div className="p-6 max-w-sm mx-auto bg-yellow-300 focus-visible rounded-xl shadow-md flex items-center space-x-4">
+           
+            <div>
+                <div className="text-xl font-medium text-black">ChitChat</div>
+                <p className="text-blue-500">hello world! {emailReducer.email}</p>
+                <input type="text" className='bg-yellow-300 border-t-0 border-l-0 border-r-0'/>
+            </div>
         </div>
     )
 }
